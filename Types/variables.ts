f404a34673cf0e660d70396800d1100f2b2be1cd
Aman07a <aman16.aa17@gmail.com>
2023-10-16 11:41:38 +0200

    Working With Numbers & Booleans

--- Types/variables.ts
+++ Types/variables.ts
@@ -1,4 +1,12 @@
 let movieTitle: string = 'Amadeus';
 movieTitle = 'Arrival';
-movieTitle = 9;
+// movieTitle = 9;
 movieTitle.toUpperCase();
+
+let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "zero";
+
+let gameOver: boolean = true;
+gameOver = true;
+// gameOver = 'true';
