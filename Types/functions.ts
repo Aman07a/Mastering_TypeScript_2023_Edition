483ad50c3334d70da259bce37332b1b3aced4213
Aman07a <aman16.aa17@gmail.com>
2023-10-16 14:11:07 +0200

    Working With Default Parameters

--- Types/functions.ts
+++ Types/functions.ts
@@ -1,14 +1,18 @@
 // Function Parameter Annotations
-const doSomething = (person: string, age: number, isFunny: boolean) => {};
-
-function greet(person: string = 'stranger'): string {
-  return `Hi there, ${person}!`;
-}
-
 function square(num: number): number {
   return num * num;
 }
 
+// function greet(person: string = 'stranger'): string {
+//   return `Hi there, ${person}!`;
+// }
+
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
 square(3);
 greet('Tonya Harding');
 doSomething('ChickenFace', 78, true);
+
+function greet(person: string = 'stranger') {
+  return `Hi there, ${person}`;
+}
