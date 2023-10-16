40a45999e9e9ad5e7ffde2a8efcbb7fadb35724b
Aman07a <aman16.aa17@gmail.com>
2023-10-16 13:10:12 +0200

    Type Inference

--- Types/variables.ts
+++ Types/variables.ts
@@ -10,3 +10,18 @@ numCatLives += 1;
 let gameOver: boolean = true;
 gameOver = true;
 // gameOver = 'true';
+
+null;
+undefined;
+
+let nothing: null = null;
+let foo: undefined = undefined;
+
+// Type Inference
+let tvShow = 'Olive Kitteridge';
+tvShow = 'The Other Two';
+// tvShow = false;
+
+let isFunny = false;
+isFunny = true;
+// isFunny = 'asd';
