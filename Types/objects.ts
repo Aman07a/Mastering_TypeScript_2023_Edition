51653e90479168253ec038df9f9cf08e315be74b
Aman07a <aman16.aa17@gmail.com>
2023-10-16 18:16:26 +0200

    Excess Properties

--- Types/objects.ts
+++ Types/objects.ts
@@ -9,3 +9,7 @@ let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 function randomCoordinate(): { x: number; y: number } {
   return { x: Math.random(), y: Math.random() };
 }
+
+// printName({ first: 'Mick', last: 'Jagger', age: 473 });
+const singer = { first: 'Mick', last: 'Jagger', age: 473, isAlive: true };
+printName(singer);
