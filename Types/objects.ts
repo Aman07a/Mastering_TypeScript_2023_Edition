ce3a54d24c6542d389441e7b832df42b4424c8c8
Aman07a <aman16.aa17@gmail.com>
2023-10-16 18:43:34 +0200

    Nested Objects

--- Types/objects.ts
+++ Types/objects.ts
@@ -34,3 +34,32 @@ function doublePoint(point: Point): Point {
 
 type MyNum = number;
 let age: MyNum = 24234;
+
+type Song = {
+  title: string;
+  artist: string;
+  numStreams: number;
+  credits: { producer: string; writer: string };
+};
+
+function calculatePayout(song: Song): number {
+  return song.numStreams * 0.0033;
+}
+
+function printSong(song: Song): void {
+  console.log(`${song.title} - ${song.artist}`);
+}
+
+const mySong: Song = {
+  title: 'Unchained Melody',
+  artist: 'Righteous Brothers',
+  numStreams: 12873321,
+  credits: {
+    producer: 'Phil Spector',
+    writer: 'Alex North',
+  },
+};
+
+const earnings = calculatePayout(mySong);
+console.log(earnings);
+printSong(mySong);
