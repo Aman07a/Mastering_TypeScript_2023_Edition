2b8cb07021bcddc292cd8eee36c756f9d8a2c174
Aman07a <aman16.aa17@gmail.com>
2023-10-16 19:01:18 +0200

    ore Array Syntax

--- Types/arrays.ts
+++ Types/arrays.ts
@@ -4,3 +4,15 @@ activeUsers.push('Tony');
 const ageList: number[] = [45, 56, 13];
 ageList[0] = 99;
 // ageList[0] = 'asd';
+
+// const bools: Array<boolean> = [];
+const bools: boolean[] = [];
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({ x: 23, y: 8 });
+// coords.push({ x: 23, y: '8' });
