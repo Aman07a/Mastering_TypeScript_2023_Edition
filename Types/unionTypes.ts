69d072bd0f1eb7c515005e855b1d289b92339171
Aman07a <aman16.aa17@gmail.com>
2023-10-17 09:27:42 +0200

    Type Narrowing w/ Union Types

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -14,3 +14,17 @@ type Loc = {
 
 let coordinates: Point | Loc = { x: 1, y: 34 };
 coordinates = { lat: 321.213, long: 23.334 };
+
+function printAge(age: number | string): void {
+  console.log(`You are ${age} years old`);
+}
+
+function calculateTax(price: number | string, tax: number): number {
+  if (typeof price === 'string') {
+    price = parseFloat(price.replace('$', ''));
+  }
+  return price * tax;
+}
+
+console.log(calculateTax(45, 0.07));
+console.log(calculateTax('$45', 0.07));
