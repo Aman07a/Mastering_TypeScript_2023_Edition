120ff196c08ce6ddfa46d9e0115e0fbb9572b216
Aman07a <aman16.aa17@gmail.com>
2023-10-17 09:20:17 +0200

    Introducing Union Types

---
+++ Types/unionTypes.ts
@@ -0,0 +1,16 @@
+let age: number | string | boolean = 21;
+age = 23;
+age = '24';
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type Loc = {
+  lat: number;
+  long: number;
+};
+
+let coordinates: Point | Loc = { x: 1, y: 34 };
+coordinates = { lat: 321.213, long: 23.334 };
