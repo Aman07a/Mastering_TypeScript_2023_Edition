3c6280b60460db2ac706edb2814ca4f98b4aba6b
Aman07a <aman16.aa17@gmail.com>
2023-10-17 10:11:43 +0200

    Introducing Interfaces

---
+++ Types/interfaces.ts
@@ -0,0 +1,13 @@
+// type Point = {
+//   x: number;
+//   y: number;
+// };
+
+// const pt: Point = { x: 213, y: 12 };
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+const pt: Point = { x: 12, y: 1234 };
