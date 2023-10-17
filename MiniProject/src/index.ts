10e0a223e220cb866d94fefef4fe74a6e69ae42c
Aman07a <aman16.aa17@gmail.com>
2023-10-17 17:40:14 +0200

    Adding in an Interface

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,18 +1,40 @@
-const btn = document.getElementById('btn')!;
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
+const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
-const form = document.getElementById('form')!;
-const list = document.getElementById('todoList')!;
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const newTodoText = input.value;
-  const newLI = document.createElement('LI');
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
+  input.value = '';
+}
+
+function createTodo(todo: Todo) {
+  const newLI = document.createElement('li');
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  newLI.append(newTodoText);
-  newLI.append(newTodoText);
+  checkbox.checked = todo.completed;
+
+  checkbox.addEventListener('change', function () {
+    todo.completed = checkbox.checked;
+  });
+
+  newLI.append(todo.text);
+  newLI.append(checkbox);
   list.append(newLI);
-  input.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
