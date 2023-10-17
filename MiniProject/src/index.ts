0fee3e52315d768ddd562cbd7f4dae1bb38a5747
Aman07a <aman16.aa17@gmail.com>
2023-10-17 17:43:04 +0200

    Connecting to LocalStorage

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -3,12 +3,19 @@ interface Todo {
   completed: boolean;
 }
 
-const todos: Todo[] = [];
-
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
 const list = document.getElementById('todolist')!;
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem('todos');
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
