e8556cb98f2b4f60938e37e8ec60f8ca6b81372b
Aman07a <aman16.aa17@gmail.com>
2023-10-17 17:04:47 +0200

    Building The Todo List

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,10 +1,18 @@
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.getElementById('form')!;
+const list = document.getElementById('todoList')!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  console.log('SUBMITTED!');
+  const newTodoText = input.value;
+  const newLI = document.createElement('LI');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  newLI.append(newTodoText);
+  newLI.append(newTodoText);
+  list.append(newLI);
+  input.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
