7bff9cfacd975aa3dbb8faa63c2b6f7da4cc39da
Aman07a <aman16.aa17@gmail.com>
2023-10-17 16:57:28 +0200

    Working With Events

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,7 +1,14 @@
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
+const form = document.getElementById('form')!;
 
-btn.addEventListener('click', function () {
-  alert(input.value);
-  input.value = '';
-});
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  console.log('SUBMITTED!');
+}
+
+form.addEventListener('submit', handleSubmit);
+// btn.addEventListener('click', function () {
+//   alert(input.value);
+//   input.value = '';
+// });
