de7aa6b45665e27aa8f5b3802030e97b72e52c4c
Aman07a <aman16.aa17@gmail.com>
2023-10-17 10:14:59 +0200

    Readonly and Optional Interface Properties

--- Types/interfaces.ts
+++ Types/interfaces.ts
@@ -11,3 +11,20 @@ interface Point {
 }
 
 const pt: Point = { x: 12, y: 1234 };
+
+interface Person {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+}
+
+const thomas: Person = {
+  first: 'Thomas',
+  last: 'Hardy',
+  nickname: 'Tom',
+  id: 21837,
+};
+
+thomas.first = 'kasjdh';
+// thomas.id = 238974;
