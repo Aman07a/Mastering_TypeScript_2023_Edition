78d47da9615a751ae35a5f0d0fd84c11fe2184d3
Aman07a <aman16.aa17@gmail.com>
2023-10-18 10:23:46 +0200

    Constructors

--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,11 +1,21 @@
 class Player {
+  first: string;
+  last: string;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+
   taunt(): void {
     console.log('BOOYAH!');
   }
 }
 
-const player1: Player = new Player();
+const player1: Player = new Player('blue', 'steele');
 player1.taunt();
+console.log(player1.first);
+console.log(player1.last);
 
-const player2: Player = new Player();
+const player2: Player = new Player('charlie', 'brown');
 player2.taunt();
