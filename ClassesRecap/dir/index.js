c94a823c6c96147dcb991be348c4cd67f05804b4
Aman07a <aman16.aa17@gmail.com>
2023-10-18 07:55:23 +0200

    The Class Keyword

--- ClassesRecap/dir/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log('ITS WOOO!!');
-console.log('YAYAYA!!!');
--- ClassesRecap/src/index.ts
+++ ClassesRecap/src/index.ts
@@ -1,2 +1,11 @@
-console.log('ITS WOOO!!');
-console.log('YAYAYA!!!');
+class Player {
+  taunt(): void {
+    console.log('BOOYAH!');
+  }
+}
+
+const player1: Player = new Player();
+player1.taunt();
+
+const player2: Player = new Player();
+player2.taunt();
